b5138da55cfb82e9bb8e3d31b1311eeacad150ae
Aditya Potluri <adityapotluri1@gmail.com>
2021-09-07 16:58:02 -0400

    binarySearch

--- binarySearch.py
+++ binarySearch.py
@@ -1,10 +1,13 @@
-def binarySearch(arr,num):
-    midNum = len(arr)//2
+def binarySearch(arr,low, high, num):
+    midNum = (high + low)//2
     if arr[midNum] == num:
         return midNum
     elif arr[midNum] > num:
-        return binarySearch(arr[0:midNum],num) if midNum else -1
-    return binarySearch(arr[midNum:],num) if midNum else -1
+        return binarySearch(arr, 0, midNum, num) 
+    elif arr[midNum] < num:
+        return binarySearch(arr, midNum, high, num)
+    else:
+        return -1
 
 
-print(binarySearch([0,1,2,3,4,5],4))
+
